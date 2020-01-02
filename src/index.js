6417f2c14978ec61ba2fa0186f72394db6da601b
Indraraj <indraraj@infiniteit.biz>
2020-01-02 12:09:24 +0530

    feat: update, delete

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-const express = require('express');
-require('./db/mongoose');
-const User = require('./models/user');
-const Task = require('./models/task');
+const express = require("express");
+require("./db/mongoose");
+const User = require("./models/user");
+const Task = require("./models/task");
 
 const app = express();
 const port = process.env.port || 4000;
 
 app.use(express.json());
 
-app.post('/users', async (req, res) => {
+app.post("/users", async (req, res) => {
   console.log(req.body);
   const user = new User(req.body);
   try {
@@ -19,7 +19,7 @@ app.post('/users', async (req, res) => {
   }
 });
 
-app.get('/users', async (req, res) => {
+app.get("/users", async (req, res) => {
   try {
     const users = await User.find({});
     res.send(users);
@@ -28,7 +28,7 @@ app.get('/users', async (req, res) => {
   }
 });
 
-app.get('/users/:id', async (req, res) => {
+app.get("/users/:id", async (req, res) => {
   try {
     const user = await User.findById(req.params.id);
     if (!user) {
@@ -40,7 +40,43 @@ app.get('/users/:id', async (req, res) => {
   }
 });
 
-app.post('/tasks', async (req, res) => {
+app.patch("/users/:id", async (req, res) => {
+  const updates = Object.keys(req.body);
+  const allowUpdates = ["name", "email", "password", "age"];
+  const isVaildUpdates = updates.every(update => {
+    return allowUpdates.includes(updates);
+  });
+
+  if (!isVaildUpdates) {
+    return res.status(422).send("Invalid updates");
+  }
+  try {
+    const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true
+    });
+    if (!user) {
+      return res.status(404).send();
+    }
+    res.send(user);
+  } catch (e) {
+    res.status(500).send(e);
+  }
+});
+
+app.delete("/users/:id", async (req, res) => {
+  try {
+    const user = await User.findByIdAndDelete(req.params.id);
+    if (!user) {
+      return res.status(404).send();
+    }
+    res.send({ ...user._doc, deleted: "deleted successfully!" });
+  } catch (e) {
+    res.status(500).send(e);
+  }
+});
+
+app.post("/tasks", async (req, res) => {
   console.log(req.body);
   const task = new Task(req.body);
   try {
@@ -51,7 +87,7 @@ app.post('/tasks', async (req, res) => {
   }
 });
 
-app.get('/tasks', async (req, res) => {
+app.get("/tasks", async (req, res) => {
   try {
     const tasks = await Task.find({});
     res.send(tasks);
@@ -60,7 +96,7 @@ app.get('/tasks', async (req, res) => {
   }
 });
 
-app.get('/tasks/:id', async (req, res) => {
+app.get("/tasks/:id", async (req, res) => {
   try {
     const task = await Task.findById(req.params.id);
     if (!task) {
@@ -72,6 +108,43 @@ app.get('/tasks/:id', async (req, res) => {
   }
 });
 
+app.patch("/tasks/:id", async (req, res) => {
+  const updates = Object.keys(req.body);
+  const allowUpdates = ["description", "completed"];
+  const isValidateUpdate = updates.every(update => {
+    return allowUpdates.includes(update);
+  });
+
+  if (!isValidateUpdate) {
+    return res.status(422).send("Invalid updates");
+  }
+
+  try {
+    const task = await Task.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true
+    });
+    if (!task) {
+      return res.status(404).send();
+    }
+    res.send(task);
+  } catch (e) {
+    res.status(500).send(e);
+  }
+});
+
+app.delete("/tasks/:id", async (req, res) => {
+  try {
+    const task = await Task.findByIdAndDelete(req.params.id);
+    if (!task) {
+      return res.status(404).send();
+    }
+    res.send({ ...task._doc, deleted: "deleted successfully!" });
+  } catch (e) {
+    res.status(500).send(e);
+  }
+});
+
 app.listen(port, () => {
-  console.log('Server is started on ' + port);
+  console.log("Server is started on " + port);
 });
